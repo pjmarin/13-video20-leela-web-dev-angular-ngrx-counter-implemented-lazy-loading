89d2c2c10d29ba346bc4944b05bbb3ca036e2d44
pjmarin <39600116+pjmarin@users.noreply.github.com>
2022-11-11 09:26:40 +0100

    implemented lazy loading

---
+++ src/app/posts/state/posts.selector.ts
@@ -0,0 +1,26 @@
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { Post } from "src/app/models/posts.model";
+import { PostsState } from "./posts.state";
+
+const getPostsState = createFeatureSelector<PostsState>("posts");
+
+export const getPosts = createSelector(getPostsState, state => {
+    return state.posts;
+});
+
+// export const getPostById = createSelector(getPostsState, (state: PostsState, props: Post): Post | undefined  => {
+//     const postFound = state.posts.find((post: Post) => post.id === props.id)
+//     return postFound;
+// });
+
+// to get rid of the deprecated warning, a solution could be - (NOT WORKING)
+
+// const getPostById = (props: { id: string }) => {
+//     return createSelector(getPostsState, (state) =>
+//         state.posts.find((post) => post.id === props.id)
+//     );
+// }
+
+export const getPostById = createSelector(getPostsState, (state: PostsState, props: Partial<Post>): Post | undefined  => {
+    return state.posts.find((post: Post) => post.id === props.id);     
+});
